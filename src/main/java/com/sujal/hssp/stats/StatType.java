b8380ac84c3667c9768f9606428b45cfc92befd0
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-02 17:40:54 +0530

    Create StatType.java

---
+++ src/main/java/com/sujal/hssp/stats/StatType.java
@@ -0,0 +1,28 @@
+package com.sujal.hssp.stats;
+
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public enum StatType {
+    HEALTH("Health", Formatting.RED, "❤"),
+    DEFENSE("Defense", Formatting.GREEN, "❈"),
+    STRENGTH("Strength", Formatting.RED, "❁"),
+    SPEED("Speed", Formatting.WHITE, "✦"),
+    CRIT_CHANCE("Crit Chance", Formatting.BLUE, "☣"),
+    CRIT_DAMAGE("Crit Damage", Formatting.BLUE, "☠"),
+    INTELLIGENCE("Intelligence", Formatting.AQUA, "✎");
+
+    public final String name;
+    public final Formatting color;
+    public final String icon;
+
+    StatType(String name, Formatting color, String icon) {
+        this.name = name;
+        this.color = color;
+        this.icon = icon;
+    }
+
+    public Text getFormatted(double value) {
+        return Text.literal((int)value + icon + " " + name).formatted(color);
+    }
+}
